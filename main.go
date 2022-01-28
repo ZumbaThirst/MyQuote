8e493c9fbcece4958872d4aae79f591410fa53bb
ZumbaThirst <markus.fjelltun@hotmail.com>
2022-01-28 20:16:46 +0100

    MyQuote

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	q "github.com/ZumbaThirst/MyQuote/myquote"
+)
+
+func main() {
+	q.PrintQuote()
+}
---
+++ myquote/go.mod
@@ -0,0 +1,5 @@
+module github.com/ZumbaThirst/MyQuote.git
+
+go 1.13
+
+require rsc.io/quote v1.5.2
